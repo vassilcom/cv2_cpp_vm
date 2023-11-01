fd78b0d112c259784c45323e7a544f8e679a345d
Vasili Macharadze <vasilcom@yahoo.com>
2023-11-02 00:43:55 +0100

    cv2 code

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,30 @@
 
 #include "interface.h"
 
-#define PY_CODE "import numpy as np\n\
-import embeddedmodule"
+#define PY_CODE_BEGIN "import numpy as np\n\
+import cv2\n\
+import embeddedmodule\n\
+capture = cv2.VideoCapture(0)\n\
+"
+#define PY_CODE_END "capture.release()\n\
+print('end of cv2')\n\
+"
 
-static char text[1024 * 16] ="pic = np.random.randint(0, high=255, size=(6, 6, 4), dtype='uint8')\n"
-                "embeddedmodule.copy3DNumpyArray(pic)";
+static char text[1024 * 16] ="\
+pic = np.random.randint(0, high=255, size=(6, 6, 4), dtype='uint8')\n\
+if(capture.isOpened()):\n\
+    ret, frame = capture.read()\n\
+    dim = (60, 60)\n\
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)\n\
+    if(ret == True):\n\
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)\n\
+        print(img)\n\
+        embeddedmodule.copy3DNumpyArray(img)\n\
+    else:\n\
+        print('no frame available')\n\
+else:\n\
+    print('no capture available. make sure that device ID is correct')\n\
+";
                 
 
 bool imagedata_to_gpu(unsigned char* image_data,  GLuint* out_texture, int image_width, int image_height)
@@ -31,6 +50,19 @@ bool imagedata_to_gpu(unsigned char* image_data,  GLuint* out_texture, int image
     *out_texture = image_texture;
 }
 
+bool exec_py()
+{
+    try
+    {
+        pybind11::exec (text);
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+}
+    
 
 int main()
 {
@@ -44,8 +76,8 @@ int main()
     // pybind11::print(sys.attr("path"));
 
     //
-    pybind11::exec (PY_CODE);
-    pybind11::exec (text);
+    pybind11::exec (PY_CODE_BEGIN);
+    exec_py();
 
     GLuint my_image_texture = 0;
     imagedata_to_gpu(myVec3D->flttend3D, &my_image_texture, myVec3D->cols, myVec3D->rows);
@@ -53,7 +85,8 @@ int main()
 
     while (my_win.loop())
     {
-        pybind11::exec (text);
+        exec_py();
+
      
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, myVec3D->cols, myVec3D->rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, myVec3D->flttend3D);
 
@@ -62,14 +95,27 @@ int main()
         bool show_demo_window = true;
         ImGui::ShowDemoWindow(&show_demo_window);
 
-        if(ImGui::Begin("text window"))
+	    ImGui::SetNextWindowPos({ 20, 350}, ImGuiCond_FirstUseEver);
+	    ImGui::SetNextWindowSize({ 700,350 }, ImGuiCond_FirstUseEver);
+        if(ImGui::Begin("python loop code"))
         {
-            ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16));
+            static char text_h[1024 * 16];
+            strcpy(text_h, text);
+            if(ImGui::InputTextMultiline("##source", text_h, IM_ARRAYSIZE(text_h), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 18),ImGuiInputTextFlags_AllowTabInput))
+            {
+            }
+
+            if(ImGui::IsItemDeactivated())
+            {
+                strcpy(text, text_h);
+            }
         }
         ImGui::End();
 
         // render frame buffer output image
-        if(ImGui::Begin("image window"))
+	    ImGui::SetNextWindowPos({ 10,10 }, ImGuiCond_FirstUseEver);
+	    ImGui::SetNextWindowSize({ 320,320 }, ImGuiCond_FirstUseEver);
+        if(ImGui::Begin("image output"))
         {
             ImVec2 pos = ImGui::GetWindowPos();
             ImVec2 size = ImGui::GetWindowSize();
@@ -87,6 +133,7 @@ int main()
         my_win.swap_buffers();
 
     }
+    pybind11::exec (PY_CODE_END);
     matrix3D_free(myVec3D);
     return 0;
 }
