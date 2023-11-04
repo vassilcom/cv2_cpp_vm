8ecedc3708dfebe0f8d7e770533aba04c1f09dba
Vasili Macharadze <vasilcom@yahoo.com>
2023-11-04 03:31:52 +0100

    full py code in editor (not pybind11)

--- src/main.cpp
+++ src/main.cpp
@@ -5,56 +5,61 @@
 #include "interface.h"
 
 #define PY_CODE_BEGIN "import numpy as np\n\
-import cv2\n\
 import embeddedmodule\n\
-capture = cv2.VideoCapture(0)\n\
 "
-#define PY_CODE_END "capture.release()\n\
-print('end of cv2')\n\
-"
-
+#define PY_CODE_END ""
+// >>> import cmake_example
+// >>> cmake_example.add(4,5)
 static char text[1024 * 16] =R"(
 pic = np.random.randint(0, high=255, size=(6, 6, 4), dtype='uint8')
-if(capture.isOpened()):
-	ret, frame = capture.read()
-	dim = (300, 300)
-	frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-
-	# uncomment it for canny edges:
-	#frame = cv2.Canny(frame, 100, 200)
-
-	if(ret == True):
-		frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-		print(frame)
-		embeddedmodule.copy3DNumpyArray(frame)
-	else:
-		print('no frame available')
-else:
-	print('no capture available. make sure that device ID is correct')
+#embeddedmodule.copy3DNumpyArray(pic)
 )";
+
+static char virt_file[1024 * 16] =R"(
+import numpy as np
+import cv2
+import embeddedmodule
+x = 42
+y = 5
+def fun1():
+	pic = np.random.randint(0, high=255, size=(6, 6, 4), dtype='uint8')
+	embeddedmodule.copy3DNumpyArray(pic)
+	print("fun1 >> dd")
+	return 2*2
+xxx=3
+print("dd")
+)";
+
                 
 ImVec4 frame_bg = ImVec4(0, 0.2, 0.2,1);
 
-
+#include <Python.h>
+PyObject* myFunction;
 bool imagedata_to_gpu(unsigned char* image_data,  GLuint* out_texture, int image_width, int image_height)
 {
-    // Create a OpenGL texture identifier
-    GLuint image_texture;
-    glGenTextures(1, &image_texture);
-    glBindTexture(GL_TEXTURE_2D, image_texture);
+	if(image_data)
+	{
+		// Create a OpenGL texture identifier
+		GLuint image_texture;
+		glGenTextures(1, &image_texture);
+		glBindTexture(GL_TEXTURE_2D, image_texture);
 
-    // Setup filtering parameters for display
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+		// Setup filtering parameters for display
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
 
-    // Upload pixels into texture
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
-    
+		// Upload pixels into texture
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+		
 
-    *out_texture = image_texture;
+		*out_texture = image_texture;
+		return true;
+
+	}
+	return false;
+
 }
 
 bool exec_py()
@@ -62,7 +67,9 @@ bool exec_py()
     try
     {
         frame_bg =  ImVec4(0, 0.2, 0.2,1);
-        pybind11::exec (text);
+        PyObject* myResult = PyObject_CallObject(myFunction, NULL);// getting result
+		float result = PyFloat_AsDouble(myResult);
+		printf("fun1: %.3f \n", result);
         return true;
     }
     catch (...)
@@ -72,6 +79,28 @@ bool exec_py()
     }
 }
     
+bool rebuild_py()
+{
+    try
+    {
+        frame_bg =  ImVec4(0, 0.2, 0.2,1);
+
+		PyObject* compiled_code = Py_CompileString(virt_file, "<string>", Py_file_input);
+		
+		PyObject* myModule = PyImport_ExecCodeModule("mymodule", compiled_code);
+		myFunction = PyObject_GetAttrString(myModule,(char*)"fun1");
+		PyObject* myResult = PyObject_CallObject(myFunction, NULL);// getting result
+		
+
+        return true;
+    }
+    catch (...)
+    {
+        frame_bg = ImVec4(0.2, 0, 0,1);
+        return false;
+    }	
+
+}
 
 int main()
 {
@@ -85,19 +114,51 @@ int main()
     // pybind11::print(sys.attr("path"));
 
     //
-    pybind11::exec (PY_CODE_BEGIN);
-    exec_py();
+    //pybind11::exec (PY_CODE_BEGIN);
+    //exec_py();
+	//PyObject* module_code = "x = 42";
+	Py_Initialize();
+
+	rebuild_py();
+	exec_py();
 
     GLuint my_image_texture = 0;
     imagedata_to_gpu(myVec3D->flttend3D, &my_image_texture, myVec3D->cols, myVec3D->rows);
 
 
+
+
+// 	const char *MyModuleName = "blah";
+// 	const char *MyModuleCode = R"(
+// print( 'Hello world!')
+// pint('ddd')
+// def fun1():
+// 	return 2*2
+// )";
+// 	PyObject *pyModule = PyModule_New(MyModuleName);
+// 	// Set properties on the new module object
+// 	PyModule_AddStringConstant(pyModule, "__file__", "");
+// 	PyObject *localDict = PyModule_GetDict(pyModule);   // Returns a borrowed reference: no need to Py_DECREF() it once we are done
+// 	PyObject *builtins = PyEval_GetBuiltins();  // Returns a borrowed reference: no need to Py_DECREF() it once we are done
+// 	PyDict_SetItemString(localDict, "__builtins__", builtins);
+
+// 	// Define code in the newly created module
+// 	PyObject *pyValue = PyRun_String(MyModuleCode, Py_file_input, localDict, localDict);
+// 	if (pyValue == NULL) {
+// 		// Handle error
+// 	}
+// 	else
+// 		Py_DECREF(pyValue);
+
+
     while (my_win.loop())
     {
+
+
         exec_py();
 
-     
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, myVec3D->cols, myVec3D->rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, myVec3D->flttend3D);
+		if(myVec3D->flttend3D)
+        	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, myVec3D->cols, myVec3D->rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, myVec3D->flttend3D);
 
         my_win.pre_render();
 
@@ -112,8 +173,9 @@ int main()
 			ImGuiIO& io = ImGui::GetIO();
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
 
-            if(ImGui::InputTextMultiline("##source", text, IM_ARRAYSIZE(text), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 22),ImGuiInputTextFlags_AllowTabInput))
+            if(ImGui::InputTextMultiline("##source", virt_file, IM_ARRAYSIZE(virt_file), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 22),ImGuiInputTextFlags_AllowTabInput))
             {
+				rebuild_py();
             }
 
         }
@@ -141,7 +203,6 @@ int main()
         my_win.swap_buffers();
 
     }
-    pybind11::exec (PY_CODE_END);
     matrix3D_free(myVec3D);
     return 0;
 }
